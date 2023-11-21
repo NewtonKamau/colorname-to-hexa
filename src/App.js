c09315342bb1f0424fb1210805cdb7f9ce84a989
NewtonKamau <kamaunewton78@gmail.com>
2023-11-21 16:20:39 +0300

    colorname to hexadecimal converter app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Input from "./Input";
+import Square from "./Square";
+
+function App () {
+  const [colorValue, setColorValue] = useState('');
+  const [hexValue, setHexValue] = useState('');
+
+  return (
+    <div className="App">
+      <Square
+        colorValue={ colorValue }
+        hexValue={hexValue}
+      />
+      <Input
+        colorValue={ colorValue }
+        setColorValue={ setColorValue }
+        setHexValue={setHexValue}
+
+      />
+   </div>
+  );
+}
+
+export default App;
